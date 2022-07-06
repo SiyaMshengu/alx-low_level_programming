f2e2c01813c42ddc6e12fb7a26963d964c41c07c
Siyabonga <Dimplezmshengu@gmail.com>
2022-07-06 07:19:03 -0700

    Task2. Update 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,14 @@
 * print_alphabet_x10 -> prints the lowercase alphabet */
 void print_alphabet_x10(void)
 { 
- int j;
- int x;
-for (x = 0; x < 10; x++)
-{
-for (j = 'a'; j <= 'z'; j++)
-{ 
-_putchar(j);
-}
-_putchar('\n');
-}
+     int j;
+     int x;
+     for (x = 0; x < 10; x++)
+     {
+         for (j = 'a'; j <= 'z'; j++)
+         {
+              _putchar(j);
+         } 
+              _putchar('\n');
+     }
 }
