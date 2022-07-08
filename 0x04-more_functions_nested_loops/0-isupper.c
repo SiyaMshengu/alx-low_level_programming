6e90c07f6d6cb7833529dab4650ca86fcd8fd3ae
Siyabonga <Dimplezmshengu@gmail.com>
2022-07-08 15:53:34 -0700

    Task0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks uppercase
+ * @c: print int
+ * Return: returns 1 or 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
