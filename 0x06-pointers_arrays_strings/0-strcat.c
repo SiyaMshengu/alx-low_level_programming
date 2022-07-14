3e525b33d40a9d3d43ddb7b4f87c1d7534f00bf1
Siyabonga <Dimplezmshengu@gmail.com>
2022-07-14 06:02:17 -0700

    Task0. super fix 3

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-int c, c2;
-c = 0;
-/* size of dest array*/
-while (dest[c])
-c++
-/* iterate through each src array value*/
-for (c2 - 0; src[c2] ; c2++)
-/* append src[c2] to dest[c]*/
-dest[c++] = src[c2];
+int x = 0, dest_len = 0;
+while (dest[x++])
+dest_len++;
+
+for (x = 0; src[x]; x++)
+dest[dest_len++] = src[x];
+
 return (dest);
 }
