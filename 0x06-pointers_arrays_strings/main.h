3396ee138527b245da886b1db057a1a759b8f3ba
Siyabonga <Dimplezmshengu@gmail.com>
2022-07-15 13:43:27 -0700

    All task fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@
  * int prototypes()
  * char prototypes()
  */
-
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
