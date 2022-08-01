e9485c1099a92810d4d7da4fdba0638fe84e0844
Siyabonga <Dimplezmshengu@gmail.com>
2022-08-01 08:41:45 -0700

    Task.o Super Fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,18 +8,14 @@
  * @owner: The owner of the dog.
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
 
-/**
- * dog_t - Typedef for struct dog
- */
+} dog_t
 
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
